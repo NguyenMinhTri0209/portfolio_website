049ad94c908628b17a6d73bd6a00559d5bba37c5
Trí Minh <minhtriarch84@gmail.com>
2026-05-24 22:32:08 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,13 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { AboutMe } from './components/AboutMe';
-import { TableOfContent } from './components/TableOfContent';
-import { Fixtek } from './components/projects/Fixtek';
-import { GachDo } from './components/projects/GachDo';
-import { CaloFix } from './components/projects/CaloFix';
-import { PhenCha } from './components/projects/PhenCha';
-import { Footer } from './components/Footer';
+import aboutMeImage from '../img/About me.png';
 
 export default function App() {
   return (
-    <div className="bg-white min-h-screen text-gray-900 selection:bg-blue-accent selection:text-white relative">
-      <div className="absolute top-0 left-0 w-full h-[80vh] bg-gradient-to-b from-[#C8DFFC] via-[#EAF2FF] to-white pointer-events-none -z-10"></div>
-      <main className="flex flex-col items-center">
-        <Header />
-        <AboutMe />
-        <TableOfContent />
-        <Fixtek />
-        <GachDo />
-        <CaloFix />
-        <PhenCha />
-      </main>
-      <Footer />
-    </div>
+    <main className="min-h-screen bg-white">
+      <img
+        src={aboutMeImage}
+        alt="About me"
+        className="block w-full h-auto"
+      />
+    </main>
   );
 }
-
